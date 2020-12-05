4b85f00b8095b892d030decddf7530c4d4d20745
WangQuanLiu <15586183797@163.com>
2020-12-05 18:34:42 +0800

    使用Box来实现菜单

--- 2.6.1/src/Menu.java
+++ 2.6.1/src/Menu.java
@@ -9,14 +9,40 @@ import javax.swing.*;
 public class Menu {
     private JFrame menuFrame;
 
-    private JPanel panelControl;
+    private JPanel panelControl=new JPanel();
+    private Box boxVer=Box.createVerticalBox(),boxHor=Box.createHorizontalBox();
     Menu(){
         iniMenu();
     }
+    void addButton(String text,String icon){
+        JButton temp=new JButton(text,createImageIcon(icon,""));
+        panelControl.add(temp);
+        menuFrame.add(panelControl);
+    }
+    void addButton(String text){
+        JButton temp=new JButton(text);
+
+        panelControl.add(temp);
+        menuFrame.add(panelControl);
+    }
     void iniMenu() {
+
         menuFrame=new JFrame("坦克大战");
-        menuFrame.setSize(1400,700);
-        menuFrame.setLayout(new GridLayout(3,1));
+        menuFrame.setSize(400,400);
+        menuFrame.setLayout(new GridLayout());
+        boxHor.setSize(200,50);
+        boxHor.add(boxVer);
+       boxVer.add(Box.createRigidArea(new Dimension(100,20)));
+       //boxVer.add(Box.createVerticalGlue());
+       boxVer.add(new JButton("开始游戏"));
+        boxVer.add(Box.createVerticalStrut(30));
+       boxVer.add(new JButton("难度选择"));
+        boxVer.add(Box.createVerticalStrut(30));
+       boxVer.add(new JButton("退出"));
+
+        menuFrame.add(boxHor);
+        menuFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
         menuFrame.setVisible(true);
     }
     private static ImageIcon createImageIcon(String path,String description){
